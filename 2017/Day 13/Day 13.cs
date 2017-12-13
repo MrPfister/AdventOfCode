ac79bb9bd798f9c94b4f6534bc5bed6ed3311e56
Kevin Pfister <yet.another.idiot@gmail.com>
2017-12-13 15:57:12 +0000

    Shortened Day 13 via enumerable range

--- 2017/Day 13/Day 13.cs
+++ 2017/Day 13/Day 13.cs
@@ -13,23 +13,9 @@ namespace AdventOfCode
         static void Main(string[] args)
         {
             // Day 13
-
-            // Part 1:
             List<List<int>> day13LUT = File.ReadAllLines(@"input.txt").Select(l => l.Replace(" ", "").Split(':').Select(t => int.Parse(t)).ToList()).ToList();
             Console.WriteLine("Severity: " + day13LUT.Sum(s => (s[0] % (s[1] * 2 - 2) == 0 ? (s[0] * s[1]) : 0)));
-
-            // Part 2:
-            int delay = 0;
-            while (true)
-            {
-                if (day13LUT.Sum(s => ((s[0] + delay) % (s[1] * 2 - 2) == 0 ? 1 : 0)) == 0)
-                {
-                    Console.WriteLine("Delay Required = " + delay);
-                    break;
-                }
-                delay++;
-            }
-
+            Console.WriteLine("Delay Required = " + Enumerable.Range(0, int.MaxValue).First(d => day13LUT.Sum(s => ((s[0] + d) % (s[1] * 2 - 2) == 0 ? 1 : 0)) == 0));
             Console.Read();
         }
     }
